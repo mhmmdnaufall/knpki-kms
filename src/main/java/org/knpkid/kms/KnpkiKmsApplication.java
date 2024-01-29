c9b0b8f7a83968dd2f0dd3fb71f4e3700255959d
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-01-29 20:44:51 +0700

    Project Init

---
+++ src/main/java/org/knpkid/kms/KnpkiKmsApplication.java
@@ -0,0 +1,13 @@
+package org.knpkid.kms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnpkiKmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnpkiKmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/knpkid/kms/KnpkiKmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.knpkid.kms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnpkiKmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
