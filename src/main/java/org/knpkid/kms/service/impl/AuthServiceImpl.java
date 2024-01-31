080e69e99af1ba967ec9ea96387bd2aa0327bb10
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-02-01 01:19:30 +0700

    Update AuthService
    melakukan autentikasi dengan AuthenticationManager

--- src/main/java/org/knpkid/kms/service/impl/AuthServiceImpl.java
+++ src/main/java/org/knpkid/kms/service/impl/AuthServiceImpl.java
@@ -2,12 +2,13 @@ package org.knpkid.kms.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.knpkid.kms.model.LoginAdminRequest;
-import org.knpkid.kms.repository.AdminRepository;
 import org.knpkid.kms.security.JwtTokenUtils;
+import org.knpkid.kms.service.AdminService;
 import org.knpkid.kms.service.AuthService;
 import org.knpkid.kms.service.ValidationService;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,31 +16,30 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    private final AdminRepository adminRepository;
+    private final AdminService adminService;
 
     private final ValidationService validationService;
 
     private final JwtTokenUtils jwtTokenUtils;
 
-    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public String getLoginToken(LoginAdminRequest request) {
         validationService.validate(request);
 
-        final var admin = adminRepository.findById(request.username())
-                .orElseThrow(this::throwRequestLoginFailed);
+        authenticate(request.username(), request.password());
 
-        if (!passwordEncoder.matches(request.password(), admin.getPassword())) {
-            throw throwRequestLoginFailed();
-        }
-
+        final var admin = adminService.loadUserByUsername(request.username());
         return jwtTokenUtils.generateToken(admin);
-
     }
 
-    private ResponseStatusException throwRequestLoginFailed() {
-        return new ResponseStatusException(HttpStatus.UNAUTHORIZED, "username or password wrong");
+    private void authenticate(String username, String password) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "username or password wrong");
+        }
     }
 
 
