51278210c3dc235a56d1ed31acf4397b1a7cd991
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-02-01 18:48:38 +0700

    Update authorization path

--- src/main/java/org/knpkid/kms/security/WebSecurityConfig.java
+++ src/main/java/org/knpkid/kms/security/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( auth ->
-                    auth.requestMatchers("/api/auth/**").permitAll()
+                    auth.requestMatchers("/api/auth/login").permitAll()
                             .anyRequest().authenticated()
                 )
                 .authenticationProvider(daoAuthenticationProvider())
