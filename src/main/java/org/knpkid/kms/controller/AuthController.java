f0f4bd0e152d5b80176656fd442506b11ff0d0d4
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-02-01 18:48:11 +0700

    Create Logout API

--- src/main/java/org/knpkid/kms/controller/AuthController.java
+++ src/main/java/org/knpkid/kms/controller/AuthController.java
@@ -1,15 +1,15 @@
 package org.knpkid.kms.controller;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.knpkid.kms.model.LoginAdminRequest;
 import org.knpkid.kms.model.WebResponse;
 import org.knpkid.kms.service.AuthService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.WebUtils;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +29,21 @@ public class AuthController {
 
         return new WebResponse<>("OK", null);
     }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        final var cookie = WebUtils.getCookie(request, "token");
+
+        if (cookie != null) {
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
+
+        return new WebResponse<>("OK", null);
+    }
+
 
 }
