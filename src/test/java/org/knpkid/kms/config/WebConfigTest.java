dd13dce282cb295ca8207beb33fb8999d216d528
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-02-03 14:33:42 +0700

    Create WebConfig unit test

---
+++ src/test/java/org/knpkid/kms/config/WebConfigTest.java
@@ -0,0 +1,25 @@
+package org.knpkid.kms.config;
+
+import org.junit.jupiter.api.Test;
+import org.knpkid.kms.resolver.AdminArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+class WebConfigTest {
+
+    private final AdminArgumentResolver adminArgumentResolver = new AdminArgumentResolver();
+
+    private final WebConfig webConfig = new WebConfig(adminArgumentResolver);
+
+    @Test
+    void addArgumentResolvers() {
+        final var listSpy = spy(new ArrayList<HandlerMethodArgumentResolver>());
+
+        webConfig.addArgumentResolvers(listSpy);
+
+        verify(listSpy).add(adminArgumentResolver);
+    }
+}
