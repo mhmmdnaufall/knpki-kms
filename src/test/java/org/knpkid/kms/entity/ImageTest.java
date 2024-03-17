03a95370b9b3ee99060f5a78149b80c70991377a
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-03-17 17:41:11 +0700

    Unit testing image entity

---
+++ src/test/java/org/knpkid/kms/entity/ImageTest.java
@@ -0,0 +1,91 @@
+package org.knpkid.kms.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageTest {
+
+    private static final Image IMAGE = new Image();
+
+    @BeforeEach
+    void setUp() {
+        IMAGE.setId(null);
+        IMAGE.setFormat(null);
+    }
+
+    @Test
+    void idGetterSetter() {
+        IMAGE.setId("imageId");
+        assertEquals("imageId", IMAGE.getId());
+    }
+
+    @Test
+    void formatGetterSetter() {
+        IMAGE.setFormat(ImageFormat.PNG);
+        assertEquals(ImageFormat.PNG, IMAGE.getFormat());
+    }
+
+    @Test
+    void testEquals() {
+        final var imageEqual = new Image();
+        assertEquals(IMAGE, imageEqual);
+
+        IMAGE.setId("imageId");
+        imageEqual.setId("imageId");
+        assertEquals(IMAGE, imageEqual);
+
+        final var imageNotEqual = new Image();
+        imageNotEqual.setId("diffId");
+        assertNotEquals(IMAGE, imageNotEqual);
+
+        IMAGE.setFormat(ImageFormat.PNG);
+        imageEqual.setFormat(ImageFormat.PNG);
+        assertEquals(IMAGE, imageEqual);
+
+        imageNotEqual.setFormat(ImageFormat.JPG);
+        assertNotEquals(IMAGE, imageNotEqual);
+
+        IMAGE.setId(null);
+        imageEqual.setId(null);
+        assertEquals(IMAGE, imageEqual);
+
+        IMAGE.setId("imageId");
+        imageEqual.setId("imageId");
+        IMAGE.setFormat(null);
+        imageEqual.setFormat(null);
+        assertEquals(IMAGE, imageEqual);
+
+    }
+
+    @Test
+    void canEqual() {
+        assertTrue(IMAGE.canEqual(new Image()));
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(hashCodeCalculate(), IMAGE.hashCode());
+
+        IMAGE.setId("imageId");
+        assertEquals(hashCodeCalculate(), IMAGE.hashCode());
+
+        IMAGE.setFormat(ImageFormat.PNG);
+        assertEquals(hashCodeCalculate(), IMAGE.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        IMAGE.setId("imageId");
+        IMAGE.setFormat(ImageFormat.PNG);
+        assertEquals("imageId.png", IMAGE.toString());
+    }
+
+
+    private int hashCodeCalculate() {
+        final var idHashCode = IMAGE.getId() == null ? 43 : IMAGE.getId().hashCode();
+        final var formatHashCode = IMAGE.getFormat() == null ? 43 : IMAGE.getFormat().hashCode();
+        return (59 + idHashCode) * 59 + formatHashCode;
+    }
+}
