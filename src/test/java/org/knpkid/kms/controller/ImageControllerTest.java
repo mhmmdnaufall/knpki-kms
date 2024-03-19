81a0978942f55a79df104f7469ca46115e7cbc2a
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-03-19 14:45:02 +0700

    Unit testing ImageController

---
+++ src/test/java/org/knpkid/kms/controller/ImageControllerTest.java
@@ -0,0 +1,48 @@
+package org.knpkid.kms.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ImageControllerTest {
+
+    private static final ImageController IMAGE_CONTROLLER = new ImageController();
+
+
+    @Test
+    void getImage() {
+
+        try (final var filesMock = mockStatic(Files.class)) {
+
+            final var imageByte = "image".getBytes();
+
+            // success
+            {
+                filesMock.when(() -> Files.readAllBytes(any())).thenReturn(imageByte);
+            }
+
+            final var imageByteArrayResponse = assertDoesNotThrow(() -> IMAGE_CONTROLLER.getImage("image.png"));
+            assertEquals(imageByte, imageByteArrayResponse);
+
+            // error
+            {
+                filesMock.when(() -> Files.readAllBytes(any())).thenThrow(IOException.class);
+            }
+
+            final var exception = assertThrows(
+                    ResponseStatusException.class,
+                    () -> IMAGE_CONTROLLER.getImage("image.png")
+            );
+
+            assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+            assertEquals("'image.png' is not found", exception.getReason());
+
+        }
+    }
+}
