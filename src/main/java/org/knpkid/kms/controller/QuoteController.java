921d439a7651729d7dc8f68990cd75544a2693a9
Muhammad Naufal <muhammadnaufaall@gmail.com>
2024-04-28 16:12:05 +0700

    fix 'create quote' API path

--- src/main/java/org/knpkid/kms/controller/QuoteController.java
+++ src/main/java/org/knpkid/kms/controller/QuoteController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.knpkid.kms.entity.Admin;
 import org.knpkid.kms.model.CreateQuoteRequest;
 import org.knpkid.kms.model.QuoteResponse;
+import org.knpkid.kms.model.WebResponse;
 import org.knpkid.kms.service.QuoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,12 +17,13 @@ public class QuoteController {
     private final QuoteService quoteService;
 
     @PostMapping(
-            path = "api/quotes",
+            path = "/api/quotes",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public QuoteResponse create(@RequestBody CreateQuoteRequest request, Admin admin) {
-        return quoteService.create(request, admin);
+    public WebResponse<QuoteResponse> create(@RequestBody CreateQuoteRequest request, Admin admin) {
+        final var quoteResponse = quoteService.create(request, admin);
+        return new WebResponse<>(quoteResponse, null, null);
     }
 
     @DeleteMapping("/api/quotes/{quoteId}")
